6c43148fb6fda62965da0d7d83cfe5bad0ba0150
ForteEscape <sehun5515@naver.com>
2020-07-01 17:22:12 +0900

    add Week4 code

--- Week4/Address.java
+++ Week4/Address.java
@@ -10,11 +10,11 @@ public class Address {
 
         System.out.println("Name : "+p1.name+", Number : "+p1.number);
 
-        Person[] members = new Person[10]; // array of class
+        Person[] members = new Person[10]; // array of class, value of array element is null
 
-        members[1] = new Person();
-        members[1].name = "David";
-        members[1].number = "22124451";
+        members[1] = new Person(); // 배열 내부로 바로 값을 넣을 수 없고 배열의 각 칸마다 Person 객체를 구현해주어야 한다.
+        members[1].name = "David"; // 배열 각각의 칸은 Person객체의 주소를 가지게 된다.
+        members[1].number = "22124451"; // Person 객체를 생성해 주지 않을 시 주소값이 null을 가리키므로 Null Point Exception이 발생한다.
 
         System.out.println("Name : "+members[1].name+", Number : "+members[1].number);
     }
---
+++ Week4/AddressMaker.java
@@ -0,0 +1,69 @@
+package Week4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class AddressMaker {
+    static Person[] members = new Person[1000];
+    static int count = 0;
+
+    public static void main(String[] args){
+        Scanner inFile;
+        try{
+            String path = "C:\\Users\\sehunkim\\Desktop\\WorkSpace\\WorkSpace\\DataStructer\\Week4\\input.txt";
+            inFile = new Scanner(new File(path));
+
+            while(inFile.hasNext()){
+                String str1 = inFile.next();
+                String str2 = inFile.next();
+
+                members[count] = new Person();
+                members[count].name = str1;
+                members[count].number = str2;
+
+                count++;
+            }
+            inFile.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("No File");
+        }
+
+        bubbleSort();
+
+        for(int i = 0; i<count; i++){
+            System.out.println("Name : "+members[i].name+" ,Number : "+members[i].number);
+        }
+    }
+
+    static void bubbleSort(){
+        for(int i = count-1; i >0; i-- ){
+            for(int j = 0; j<i; j++){
+                if(members[j].name.compareTo(members[j+1].name) > 0){
+                    swap(j, j+1);
+                }
+            }
+        }
+    }
+
+    static void swap(int num1, int num2){
+        /*String temp_name;
+        String temp_number;
+
+        temp_name = members[num1].name;
+        temp_number = members[num1].number;
+
+        members[num1].name = members[num2].name;
+        members[num1].number = members[num2].number;
+
+        members[num2].name = temp_name;
+        members[num2].number = temp_number;*/
+
+        Person temp = new Person();
+
+        temp = members[num1];
+        members[num1] = members[num2]; //members 내부의 값들은 각 객체의 주소를 가지고 있으므로 이를 swap 하기만 해도 내부 값은 바뀔 것이다.
+        members[num2] = temp;
+    }
+}
