30ae64a26c9f94d4015d30f029280ee7d58eb025
ForteEscape <sehun5515@naver.com>
2020-09-03 12:28:16 +0900

    modift ans code

--- DataStructor_class/Week2/Problem4.c
+++ DataStructor_class/Week2/Problem4.c
@@ -1,26 +1,58 @@
 #include<stdio.h>
 #include<string.h>
-#define size 100
+#include<stdlib.h>
+#include<ctype.h>
+#pragma warning(disable : 4996);
+#define SIZE 100
+#define WORD_SIZE 21
+
+char* strlwr(char* str);
 
 int main()
 {
-    char* words[size] = {0, };
-    char buffer[size];
-    char tempBuf[size];
+    char* words[SIZE] = { 0, };
+    char buffer[WORD_SIZE];
     int idx = 0;
+    int count = 0;
+    int diff_flag = 1;
 
-    while(1){
+    while (1) {
         scanf("%s", buffer);
 
-        if(strcmp(buffer, "EOF")){
-            
+        if (strcmp(buffer, "EOF") == 0) {
+            printf("%d\n", count);
+            break;
         }
-        strcpy(buffer, tempBuf);
-        words[idx] = tempBuf;
+
+        words[idx] = strdup(strlwr(buffer));
+
+        for (int i = 0; i < idx; i++) {
+            if (strcmp(words[i], strlwr(buffer)) == 0) {
+                diff_flag = 0;
+                break;
+            }
+            diff_flag = 1;
+        }
+
+        if (diff_flag == 1) {
+            count++;
+        }
+
         idx++;
     }
 
-    for(int i = 0; i<idx; i++){
-        printf("%s\n", words[i]);
+    for (int i = 0; i < idx; i++) {
+        free(words[i]);
     }
+}
+
+char* strlwr(char* str) {
+    unsigned char* p = (unsigned char*)str;
+
+    while (*p) {
+        *p = tolower((unsigned char)*p);
+        p++;
+    }
+
+    return str;
 }
